00d315d80190e2a7e1134b01e80a8f69e8eaae6d
PurplePanda95 <epneuwalder@gmail.com>
2023-08-14 16:58:07 -0400

    Added util function for avg alignment

--- include/utils.h
+++ include/utils.h
@@ -5,6 +5,7 @@
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/decimate.h>
 #include <Eigen/Core>
+#include <igl/circulation.h>
 
 #include <iostream>
 #include <string>
@@ -31,5 +32,15 @@ double angle_between_vectors(const Eigen::RowVector3d& v,
 double alignment_function(const Eigen::RowVector3d& v,
 													const Eigen::RowVector3d& w,
 													const Eigen::RowVector3d& p_edge);
-
+void compute_before(const int e,
+	const Eigen::MatrixXd& V,
+	const Eigen::MatrixXi & E,
+	const Eigen::MatrixXi & F,
+	const Eigen::VectorXi & EMAP,
+	const Eigen::MatrixXi & EF,
+	const Eigen::MatrixXi & EI, 
+	const Eigen::MatrixXi & v,//frame field 1s
+	const Eigen::MatrixXi & w,//frame field 2s
+	int & valenceSum, 
+  	double & alignment);
 #endif
