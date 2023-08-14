faaba80d2bec58461885d576cdd01a84e5068fbb
Kyle O <onghaik@gmail.com>
2023-08-14 17:12:24 -0400

    First proper draft of optimal_collapse_edge_callbacks.cpp.  Added pre-collapse and post-collapse, but still need to work on cost-and-placement.

--- optimal_collapse_edge_callbacks.cpp
+++ optimal_collapse_edge_callbacks.cpp
@@ -1,111 +1,109 @@
+#include "igl/circulation.h"
 #include "igl/decimate_callback_types.h"
+#include "igl/shortest_edge_and_midpoint.h"
+#include <Eigen/src/Core/Matrix.h>
 
-std::vector<int> V2Fe;
+void optimal_collapse_edge_callbacks(
+    Eigen::MatrixXi &E,
+    /* std::vector<std::tuple<Eigen::MatrixXd, Eigen::RowVectorXd, double>>
+     */
+    /*     &quadrics, */
+    Eigen::MatrixXd &PD1, Eigen::MatrixXd &PD2, Eigen::VectorXd &PV1,
+    Eigen::VectorXd &PV2, int &v1, int &v2,
+    igl::decimate_cost_and_placement_callback &cost_and_placement,
+    igl::decimate_pre_collapse_callback &pre_collapse,
+    igl::decimate_post_collapse_callback &post_collapse) {
+  /* typedef std::tuple<Eigen::MatrixXd, Eigen::RowVectorXd, double> Quadric; */
+  cost_and_placement = igl::shortest_edge_and_midpoint;
+  /* [&PD1, &PD2, &PV1, &PV2, &v1, &v2]( */
+  //     const int e, const Eigen::MatrixXd &V, const Eigen::MatrixXi & /*F*/,
+  //     const Eigen::MatrixXi &E, const Eigen::VectorXi & /*EMAP*/,
+  //     const Eigen::MatrixXi & /*EF*/, const Eigen::MatrixXi & /*EI*/,
+  /*     double &cost, Eigen::RowVectorXd &p) { */
+  /* // Combined quadric */
+  /* Quadric quadric_p; */
+  /* quadric_p = quadrics[E(e, 0)] + quadrics[E(e, 1)]; */
+  /* // Quadric: p'Ap + 2b'p + c */
+  /* // optimal point: Ap = -b, or rather because we have row vectors:
+   * pA=-b */
+  /* const auto &A = std::get<0>(quadric_p); */
+  /* const auto &b = std::get<1>(quadric_p); */
+  /* const auto &c = std::get<2>(quadric_p); */
+  /* p = -b * A.inverse(); */
+  /* cost = p.dot(p * A) + 2 * p.dot(b) + c; */
+  /* // Force infs and nans to infinity */
+  /* if (std::isinf(cost) || cost != cost) { */
+  /*   cost = std::numeric_limits<double>::infinity(); */
+  /*   // Prevent NaNs. Actually NaNs might be useful for debugging. */
+  /*   p.setConstant(0); */
+  /* } */
+  /* }; */
+  // Remember endpoints
+  pre_collapse =
+      [&v1, &v2](const Eigen::MatrixXd &,                             /*V*/
+                 const Eigen::MatrixXi &,                             /*F*/
+                 const Eigen::MatrixXi &E, const Eigen::VectorXi &,   /*EMAP*/
+                 const Eigen::MatrixXi &,                             /*EF*/
+                 const Eigen::MatrixXi &,                             /*EI*/
+                 const igl::min_heap<std::tuple<double, int, int>> &, /*Q*/
+                 const Eigen::VectorXi &,                             /*EQ*/
+                 const Eigen::MatrixXd &,                             /*C*/
+                 const int e) -> bool {
+    v1 = E(e, 0);
+    v2 = E(e, 1);
+    return true;
+  };
+  // update quadric
+  post_collapse = [&v1, &v2, &PD1, &PD2, &PV1, &PV2](
+                      const Eigen::MatrixXd &,     /*V*/
+                      const Eigen::MatrixXi &F,    /*F*/
+                      const Eigen::MatrixXi &,     /*E*/
+                      const Eigen::VectorXi &EMAP, /*EMAP*/
+                      const Eigen::MatrixXi &EF,   /*EF*/
+                      const Eigen::MatrixXi &EI,   /*EI*/
+                      const igl::min_heap<std::tuple<double, int, int>> &, /*Q*/
+                      const Eigen::VectorXi &, /*EQ*/
+                      const Eigen::MatrixXd &, /*C*/
+                      const int e,             /*e*/
+                      const int,               /*e1*/
+                      const int,               /*e2*/
+                      const int,               /*f1*/
+                      const int,               /*f2*/
+                      const bool collapsed) -> void {
+    std::vector<int> neighbor_vertices, neighbor_faces;
+    igl::circulation(e, false, F, EMAP, EF, EI, neighbor_vertices,
+                     neighbor_faces);
+    Eigen::RowVectorXd avg_neighbor_PD1(neighbor_vertices.size()),
+        avg_neighbor_PD2(neighbor_vertices.size());
+    double avg_neighbor_PV1 = 0;
+    double avg_neighbor_PV2 = 0;
 
-const igl::decimate_pre_collapse_callback custom_pre_collapse_callback =
-    [&V2Fe](const Eigen::MatrixXd &V, const Eigen::MatrixXi &F,
-            const Eigen::MatrixXi &E, const Eigen::VectorXi &EMAP,
-            const Eigen::MatrixXi &EF, const Eigen::MatrixXi &EI,
-            const igl::min_heap<std::tuple<double, int, int>> &Q,
-            const Eigen::VectorXi &EQ, const Eigen::MatrixXd &C,
-            const int e) -> bool {
-  V2Fe = igl::circulation(e, true, EMAP, EF, EI);
-  return true;
-};
+    for (int i = 0; i < neighbor_vertices.size(); i++) {
+      for (int j = 0; j < PD1.cols(); j++) {
+        avg_neighbor_PD1(j) += PD1(neighbor_vertices[i], j);
+        avg_neighbor_PD2(j) += PD2(neighbor_vertices[i], j);
+      }
+      avg_neighbor_PV1 += PV1(neighbor_vertices[i]);
+      avg_neighbor_PV2 += PV2(neighbor_vertices[i]);
+    }
 
-const igl::decimate_post_collapse_callback custom_post_collapse_callback =
-    [&PD1, &PD2, &PV1, &PV2,
-     &V2Fe](const Eigen::MatrixXd &V, const Eigen::MatrixXi &F,
-            const Eigen::MatrixXi &E, const Eigen::VectorXi &EMAP,
-            const Eigen::MatrixXi &EF, const Eigen::MatrixXi &EI,
-            const igl::min_heap<std::tuple<double, int, int>> &Q,
-            const Eigen::VectorXi &EQ, const Eigen::MatrixXd &C, const int e,
-            const int e1, const int e2, const int f1, const int f2,
-            const bool collapsed) -> bool {
-  if (!collapsed)
-    return false;
+    avg_neighbor_PD1 /= neighbor_vertices.size();
+    avg_neighbor_PD2 /= neighbor_vertices.size();
+    avg_neighbor_PV1 /= neighbor_vertices.size();
+    avg_neighbor_PV2 /= neighbor_vertices.size();
 
-  int v1 = E(e, 0);
-  int v2 = E(e, 1);
-
-  PD1.row(v1) = 0.5 * (PD1.row(v1) + PD1.row(v2));
-  PV1.row(v1) = 0.5 * (PV1.row(v1) + PV1.row(v2));
-  PD2.row(v1) = 0.5 * (PD2.row(v1) + PD2.row(v2));
-  PV2.row(v1) = 0.5 * (PV2.row(v1) + PV2.row(v2));
-
-  // 4 normalized vectors: PD1.row(v1), PD2.row(v1), - PD1.row(v1), -
-  // PD2.row(v1)
-  std::vector<Eigen::VectorXd> Dir = {
-      PD1.row(v1).normalized(), PD2.row(v1).normalized(),
-      -PD1.row(v1).normalized(), -PD2.row(v1).normalized()};
-
-  // store max alignment -> pair (max_aligment, (edge_index, if_flipped))
-  std::vector<std::pair<double, std::pair<int, bool>>> max_alignment = {
-      std::make_pair(-1, std::make_pair(-1, false)),
-      std::make_pair(-1, std::make_pair(-1, false)),
-      std::make_pair(-1, std::make_pair(-1, false)),
-      std::make_pair(-1, std::make_pair(-1, false))};
-
-  //// first get the edges of each face in V2Fe
-  for (int f : V2Fe) {
-    if (f == f1 || f == f2 || f >= F.rows())
-      continue;
-
-    for (int i = 0; i < 3; i++) {
-      int ei = EMAP(f * 3 + i); // Need to Make sure this indexing is right
-      if (E(ei, 0) == v1 ||
-          E(ei, 1) == v1) // the edge is already there, no flip to generate it
-      {
-        // just making vs always v1 and ve the other vertex
-        int vs = v1;
-        int ve = E(ei, 1);
-        if (E(ei, 1) == v1)
-          ve = E(ei, 0);
-
-        Eigen::Vector3d vs_e_vec = V.row(ve) - V.row(vs);
-        vs_e_vec.normalize();
-
-        for (int j = 0; j < 4; j++) {
-          double alignment = vs_e_vec.dot(Dir[j]);
-          if (alignment > max_alignment[j].first) {
-            max_alignment[j].first = alignment;
-            max_alignment[j].second.first = ei;
-            max_alignment[j].second.second = false;
-          }
-        }
-      } else // the edge is not there, flip to generate it
-      {
-        int vs = v1;
-        // ve would be the on the other side of the edge, first get the face:
-        int f_opposite = EF(ei, 0);
-        if (f_opposite == f)
-          f_opposite = EF(ei, 0);
-
-        if (f_opposite >= F.rows())
-          continue;
-
-        // now get the vertex that is not E(ei, 0) or E(ei, 1)
-        int ve = F(f_opposite, 0);
-        if (ve == E(ei, 0) || ve == E(ei, 1))
-          ve = F(f_opposite, 1);
-        if (ve == E(ei, 0) || ve == E(ei, 1))
-          ve = F(f_opposite, 2);
-
-        Eigen::Vector3d vs_e_vec = V.row(ve) - V.row(vs);
-        vs_e_vec.normalize();
-
-        for (int j = 0; j < 4; j++) {
-          double alignment = vs_e_vec.dot(Dir[j]);
-          if (alignment > max_alignment[j].first) {
-            max_alignment[j].first = alignment;
-            max_alignment[j].second.first = ei;
-            max_alignment[j].second.second = true;
-          }
-        }
+    if (v1 < v2) {
+      for (int j = 0; j < PD1.cols(); j++) {
+        PD1(v1, j) = avg_neighbor_PD1(j);
+        PD2(v1, j) = avg_neighbor_PD2(j);
+      }
+    } else {
+      for (int j = 0; j < PD1.cols(); j++) {
+        PD1(v2, j) = avg_neighbor_PD1(j);
+        PD2(v2, j) = avg_neighbor_PD2(j);
       }
     }
-  }
-  return true;
-};
-
-igl::decimate_stopping_condition_callback stopping_condition_callback;
+    PV1(v1 < v2 ? v1 : v2) = avg_neighbor_PV1;
+    PV2(v1 < v2 ? v1 : v2) = avg_neighbor_PV2;
+  };
+}
